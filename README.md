30f461af6deedbc89e2a3460429ced215c3076e9
unknown <mor.kapronczay@gmail.com>
2019-07-31 14:24:32 +0200

    organizing and progress

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cec2019
 Materials for CEC 2019 Data Science in Business
+
+To start:
+
+`pip install -r requirements.txt`
+
+`python install_reqs.py`
---
+++ install_reqs.py
@@ -0,0 +1,11 @@
+import nltk
+
+if __name__ == '__main__':
+    
+    ## NLTK resources
+    nltk.download('stopwords')
+    nltk.download('punkt')
+    nltk.download('wordnet')
+    nltk.download('averaged_perceptron_tagger')
+
+    print('\n######################\n#                    #\n#\tReady!       #\n#                    #\n######################')
